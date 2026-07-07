ed83309533957b34e7c2a611ed7da8990ff20a36
Viet Phi Long Le <levietphilong2004@gmail.com>
2026-07-08 00:04:43 +1000

    save regular

---
+++ Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+	[Header("VFX Prefabs")]
+	[SerializeField] private GameObject _swordSlashPrefab;
+
+	public GameObject playerPrefab;
+	public GameObject enemyPrefab;
+
+	public Transform playerBattleStation;
+	public Transform enemyBattleStation;
+
+	Unit playerUnit;
+	Unit enemyUnit;
+
+	public TMP_Text dialogueText;
+	public BattleHUD playerHUD;
+	public BattleHUD enemyHUD;
+
+	public BattleState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		state = BattleState.START;
+		StartCoroutine(SetupBattle());
+    }
+
+	IEnumerator SetupBattle()
+	{
+		GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+		playerUnit = playerGO.GetComponent<Unit>();
+
+		GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+		enemyUnit = enemyGO.GetComponent<Unit>();
+
+		dialogueText.text = "A wild " + enemyUnit.unitName + " approaches...";
+
+		playerHUD.SetHUD(playerUnit);
+		enemyHUD.SetHUD(enemyUnit);
+
+		yield return new WaitForSeconds(2f);
+
+		state = BattleState.PLAYERTURN;
+		PlayerTurn();
+	}
+
+	IEnumerator PlayerAttack()
+{
+    dialogueText.text = "You swing your sword!";
+
+    // 1. Spawn the sword prefab at the enemy's battle station (or player's, depending on your setup)
+    if (_swordSlashPrefab != null)
+    {
+        GameObject slashGO = Instantiate(_swordSlashPrefab, enemyBattleStation.position, Quaternion.identity);
+        SwordSlashEffect slashEffect = slashGO.GetComponent<SwordSlashEffect>();
+        
+        if (slashEffect != null)
+        {
+            // Pass the player's damage dynamically to the sword's hitbox logic
+            slashEffect.Initialize(playerUnit.damage);
+        }
+    }
+
+    // 2. Wait for the slash duration and physics matrix calculation to settle
+    yield return new WaitForSeconds(0.5f);
+
+    // 3. Update HUD values post-collision
+    enemyHUD.SetHP(enemyUnit.currentHP);
+    dialogueText.text = "The attack is successful!";
+
+    yield return new WaitForSeconds(1.5f);
+
+    // 4. Perform the structural round-state check
+    if (enemyUnit.currentHP <= 0)
+    {
+        state = BattleState.WON;
+        EndBattle();
+    } 
+    else
+    {
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
+}
+
+	IEnumerator EnemyTurn()
+	{
+		dialogueText.text = enemyUnit.unitName + " attacks!";
+
+		yield return new WaitForSeconds(1f);
+
+		bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+		playerHUD.SetHP(playerUnit.currentHP);
+
+		yield return new WaitForSeconds(1f);
+
+		if(isDead)
+		{
+			state = BattleState.LOST;
+			EndBattle();
+		} else
+		{
+			state = BattleState.PLAYERTURN;
+			PlayerTurn();
+		}
+
+	}
+
+	void EndBattle()
+	{
+		if(state == BattleState.WON)
+		{
+			dialogueText.text = "You won the battle!";
+		} else if (state == BattleState.LOST)
+		{
+			dialogueText.text = "You were defeated.";
+		}
+	}
+
+	void PlayerTurn()
+	{
+		dialogueText.text = "Choose an action:";
+	}
+
+	IEnumerator PlayerHeal()
+	{
+		playerUnit.Heal(5);
+
+		playerHUD.SetHP(playerUnit.currentHP);
+		dialogueText.text = "You feel renewed strength!";
+
+		yield return new WaitForSeconds(2f);
+
+		state = BattleState.ENEMYTURN;
+		StartCoroutine(EnemyTurn());
+	}
+
+	public void OnAttackButton()
+	{
+		if (state != BattleState.PLAYERTURN)
+			return;
+
+		StartCoroutine(PlayerAttack());
+	}
+
+	public void OnHealButton()
+	{
+		if (state != BattleState.PLAYERTURN)
+			return;
+
+		StartCoroutine(PlayerHeal());
+	}
+
+}
