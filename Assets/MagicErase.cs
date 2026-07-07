760dd2e74868ae35494fd951b318653c3da0760d
Nekoyaki <tstglitch@gmail.com>
2026-07-07 20:33:31 +1000

    update destroyable object

--- Assets/MagicErase.cs
+++ Assets/MagicErase.cs
@@ -1,34 +1,12 @@
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
-public class TileDestroyer : MonoBehaviour
+public class DestructibleTile : MonoBehaviour
 {
-    public Tilemap targetTilemap; 
-
-    // Update is called once per frame by Unity automatically
-
-    void Update()
+    void OnMouseDown()
     {
-        // Detect a left mouse button click
-        if (Input.GetMouseButtonDown(0))
-        {
-            // Get mouse position
-            Vector3 mousePosition = Input.mousePosition;
+        if (TurnManager.Instance.IsGameOver) return;
 
-            mousePosition.z = Mathf.Abs(Camera.main.transform.position.z);
-
-            // Change mouse screen position to World position
-            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePosition);
-            
-            // Change World coordinate to Tilemap cell coordinate
-            Vector3Int coordinate = targetTilemap.WorldToCell(mouseWorldPos);
-
-            // Check if a tile exists at the spot
-            if (targetTilemap.HasTile(coordinate))
-            {
-                // Erase the tile
-                targetTilemap.SetTile(coordinate, null);
-            }
-        }
+        TurnManager.Instance.UseTurn();
+        Destroy(gameObject);
     }
 }
